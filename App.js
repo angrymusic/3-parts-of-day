e480cabffaf727be76d7392fab495c0a5a81c6ce
angrymusic <angrymusic@naver.com>
2023-04-04 12:00:33 +0900

    버튼으로 현재 날짜 변경 구현

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import "react-native-gesture-handler";
 import { useState, useEffect } from "react";
-import { StyleSheet, Text, Button, View, SafeAreaView, ScrollView } from "react-native";
+import { StyleSheet, Text, Button, View, SafeAreaView, ScrollView, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { AntDesign } from "@expo/vector-icons";
 import { Drawer } from "react-native-drawer-layout";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Todo from "./components/Todo";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 const STORAGE_TODOS_KEY = "@todos";
 const STORAGE_GOALS_KEY = "@goals";
@@ -16,8 +17,7 @@ export default function App() {
     const [todos, setTodos] = useState({});
     const [goals, setGoals] = useState({});
     const [todayTodo, setTodayTodo] = useState({});
-    const [yesterdayTodo, setYesterdayTodo] = useState({});
-    const [tomorrowTodo, setTomorrowTodo] = useState({});
+    const [dayCursor, setDayCursor] = useState();
 
     const saveTodos = async (data) => {
         try {
@@ -36,7 +36,6 @@ export default function App() {
             console.log(e);
         }
     };
-    const matchTodos = () => {};
     const saveGoals = async (data) => {
         try {
             await AsyncStorage.setItem(STORAGE_GOALS_KEY, JSON.stringify(data));
@@ -61,8 +60,10 @@ export default function App() {
             console.log(newTodo);
         }
     };
-    const getDay = () => {
-        const today = new Date();
+    const getDay = (today) => {
+        if (!today) {
+            today = new Date();
+        }
         const week = ["일", "월", "화", "수", "목", "금", "토"];
 
         const todayYear = today.getFullYear();
@@ -72,10 +73,19 @@ export default function App() {
 
         setToday(todayYear + "년 " + todayMonth + "월 " + todayDate + "일 " + todayWeek + "요일");
     };
-
+    const goTomorrow = () => {
+        setDayCursor(new Date(dayCursor.setDate(dayCursor.getDate() + 1)));
+    };
+    const goYesterday = () => {
+        setDayCursor(new Date(dayCursor.setDate(dayCursor.getDate() - 1)));
+    };
     useEffect(() => {
-        getDay();
+        setDayCursor(new Date());
+        getDay(dayCursor);
     }, []);
+    useEffect(() => {
+        getDay(dayCursor);
+    }, [dayCursor]);
 
     return (
         <Drawer
@@ -94,23 +104,24 @@ export default function App() {
             <StatusBar style="black" />
             <SafeAreaView style={styles.topBar}>
                 <Text style={styles.title}>{today}</Text>
-
-                <AntDesign
-                    style={styles.sideButton}
-                    onPress={() => setOpen((prevOpen) => !prevOpen)}
-                    name="menu-unfold"
-                    size={26}
-                    color="black"
-                />
+                <TouchableOpacity>
+                    <AntDesign
+                        style={styles.sideButton}
+                        onPress={() => setOpen((prevOpen) => !prevOpen)}
+                        name="menu-unfold"
+                        size={26}
+                        color="black"
+                    />
+                </TouchableOpacity>
             </SafeAreaView>
             <View style={styles.todoWrapper}>
-                <View style={styles.arrowButton}>
+                <TouchableOpacity style={styles.arrowButton} onPress={goYesterday}>
                     <AntDesign style={styles.arrowButton} name="left" size={24} color="black" />
-                </View>
+                </TouchableOpacity>
                 <Todo todo={todayTodo} addTodo={addTodo} />
-                <View style={styles.arrowButton}>
+                <TouchableOpacity style={styles.arrowButton} onPress={goTomorrow}>
                     <AntDesign style={styles.arrowButton} name="right" size={24} color="black" />
-                </View>
+                </TouchableOpacity>
             </View>
         </Drawer>
     );
